06a33912b37a71c4582460d5d1627cc02220d4e0
JPB <jbanas9124@gmail.com>
2021-03-15 23:34:03 -0500

    starting search bar api connection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,12 @@
 
+let apiKey = '3d15e45d12f197c35af3d283e17262ae';
 
+let city = 'Nashville';
 
-/* AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly */
+getCityWeather = () => {
+  
+  let test = fetch('http://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=3d15e45d12f197c35af3d283e17262ae');
+  console.log(test);
+}
 
-/* GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city */
+getCityWeather();
